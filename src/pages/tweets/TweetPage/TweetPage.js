7112e930da31658566b6d186ac856e95fd03d331
Javier <mastos44@gmail.com>
2023-11-29 22:38:27 +0100

    User Params

--- src/pages/tweets/TweetPage/TweetPage.js
+++ src/pages/tweets/TweetPage/TweetPage.js
@@ -1,11 +1,13 @@
+import { useParams } from 'react-router-dom';
 import Content from '../../../components/layout/Content';
 
 function TweetPage() {
+  const params = useParams();
   return (
     <Content title="Tweet detail">
-      <div>Tweet detail goes here...</div>
+      <div>Tweet detail {params.tweetId} goes here...</div>
     </Content>
-  );
+  ); 
 }
 
 export default TweetPage;
